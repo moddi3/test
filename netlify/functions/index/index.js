b1f77290bc3fa7bfbb843bd7c256c50b4de4c369
Vlad Ivanov <v.ivanov@sendpulse.com>
2023-09-01 17:21:28 +0300

    fix

--- netlify/functions/index/index.js
+++ netlify/functions/index/index.js
@@ -1,17 +1,5 @@
-// for a full working demo check https://express-via-functions.netlify.com/.netlify/functions/serverless-http
-var serverless = require('serverless-http')
+const serverless = require('serverless-http');
+const server = require('../../../dist/contstructor-with-isr/server/main');
 
-var expressApp = require('../../../dist/contstructor-with-isr/server/main')
-
-module.exports = server.app();
-
-// We need to define our function name for express routes to set the correct base path
-var functionName = 'serverless-http'
-
-// Initialize express app
-var app = expressApp(functionName)
-
-// Export lambda handler
-var handler = serverless(app);
-
-module.exports = { handler: handler };
+module.exports = server.app;
+module.exports.handler = serverless(server.app);
