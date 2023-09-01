92b651af026b8a5dcc1bc4e624120c11d7eaa494
Vlad Ivanov <v.ivanov@sendpulse.com>
2023-09-01 17:28:03 +0300

    remove func add plugin

--- netlify/functions/index/index.js
+++
@@ -1,5 +0,0 @@
-const serverless = require('serverless-http');
-const server = require('../../../dist/contstructor-with-isr/server/main');
-
-module.exports = server.app;
-module.exports.handler = serverless(server.app);
